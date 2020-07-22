6a2eca2d1f1c5a692aff07f956c641c4f23a9c61
kroen3n <67289992+kroen3n@users.noreply.github.com>
2020-07-22 20:35:42 +0300

    Create mysql_login.py

---
+++ deutsche_D0k/c0de/Fall_erstellen/mysql_login.py
@@ -0,0 +1,24 @@
+from script_cred import UserDBA
+from script_cred import PasswdDBA
+from script_cred import HostDBA
+from script_cred import DBA
+
+import pymysql
+from sqlalchemy import create_engine
+
+#Datenbank einloggen
+
+class ConnectMySQL:
+    connection_dba = pymysql.connect(host=HostDBA.hostname,
+        user=UserDBA.name,
+        password=PasswdDBA.secret,
+        db=DBA.database)
+
+    db_data='mysql+mysqldb://'+'{}'.format(UserDBA.name)+':'+'{}'.format(PasswdDBA.secret)+'@'+'{}'.format(HostDBA.hostname)+':3306/'+'{}'.format(DBA.database)+'?charset=utf8mb4'
+    engine=create_engine(db_data)
+
+    #get_it=connection_dba.cursor()
+
+
+#oo = ConnectMySQL()
+#print(oo.engine)
