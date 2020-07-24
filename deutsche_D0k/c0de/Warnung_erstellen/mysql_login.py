2f250feeb5648128ea3febcd1e86d13dff8f70c1
kroen3n <67289992+kroen3n@users.noreply.github.com>
2020-07-24 23:00:27 +0300

    Update mysql_login.py

--- deutsche_D0k/c0de/Warnung_erstellen/mysql_login.py
+++ deutsche_D0k/c0de/Warnung_erstellen/mysql_login.py
@@ -17,7 +17,7 @@ class ConnectMySQL:
     db_data='mysql+mysqldb://'+'{}'.format(UserDBA.name)+':'+'{}'.format(PasswdDBA.secret)+'@'+'{}'.format(HostDBA.hostname)+':3306/'+'{}'.format(DBA.database)+'?charset=utf8mb4'
     
     engine=create_engine(db_data)
-    #get_it=connection_dba.cursor()
+    get_it=connection_dba.cursor()
 
 #oo = ConnectMySQL()
 #print(oo.engine)
