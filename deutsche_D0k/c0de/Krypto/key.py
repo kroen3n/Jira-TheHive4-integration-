3598a31e9dbbbad5dddece4d6f1febc447d90c52
kroen3n <67289992+kroen3n@users.noreply.github.com>
2020-07-26 18:47:26 +0300

    Create key.py

---
+++ deutsche_D0k/c0de/Krypto/key.py
@@ -0,0 +1,10 @@
+import os
+
+class Key:
+        
+    with open('/app/refactoring_code/classes/key.txt', 'rb') as key_read:
+        for line in key_read:
+            keyline=line
+
+
+#print(Key.keyline)
